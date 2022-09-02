8a3003d458384258a63a96e9db3ee3eb83c4358e
Golban-Julia <golden95juli@gmail.com>
2022-09-02 20:06:37 +0300

    Added edits

--- js/script.js
+++ js/script.js
@@ -11,25 +11,24 @@ const API_URL = "https://jsonplaceholder.typicode.com/users";
 
 
 fetch(API_URL)
-    .then((response) => response.json())
-    .then((result) => console.log(result.filter((user) => {
-    return user.company.name === "Johns Group"
-})))
-    .catch((error) => console.error(error))
-
-
+  .then((response) => response.json())
+  .then((users) => {
+   const result = users.find(({ company: { name } }) => name === "Johns Group")
+   console.log(result)
+  })
     
-    const getUser = async () => {
-  try {
+  .catch((error) => console.error(error))
+
+        
+const getUser = async () => {
+try {
     const response = await fetch(API_URL);
-    const result = await response.json();
-    console.log(result.filter((user) => {
-    return user.company.name === "Johns Group"
-}));
+    const users = await response.json();
+    const result = users.find(({ company: { name } }) => name === "Johns Group")
+    console.log(result)
+  
   } catch (error) {
     console.log(error);
   }
 };
-
 getUser();
- 
